34fc6f69a2c8d817d24c86732b614a61b6e2c41c
参商 <a07033686263@gmail.com>
2025-06-01 10:17:53 +0900

    Create variant_learning02.java

---
+++ chapter_01variable/variant_learning02.java
@@ -0,0 +1,19 @@
+public class variant_learning02{
+
+	public static void main(String[] args){
+    //
+    
+		int age = 20;
+		double score = 88.9;
+		char gender = 'M';//char is a basic data type in Java,used to represent a single character
+						  //with ''
+		String name = "king";
+
+		//print message
+		System.out.println("The one is");
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(score);
+	}
+}
