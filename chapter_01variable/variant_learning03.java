2365f1e97c26d4b6f1e5eab2d157037a55e8bf43
参商 <a07033686263@gmail.com>
2025-06-01 10:27:53 +0900

    Update variant_learning03.java

--- chapter_01variable/variant_learning03.java
+++ chapter_01variable/variant_learning03.java
@@ -2,9 +2,13 @@ public class variant_learning04{
 
 	public static void main(String[] args){
 		//how to use the "+" operator
+		
 		System.out.println(100 + 98);//198
+		
 		System.out.println("100" + 98 + 3);//1983
+		
 		//if string type in the formula it will put them together,e.g. 
+		
 		//when one of the operands is a string,java treats the "+" as a string concatenation operator
 	}
 }
