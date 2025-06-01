f23dc7ee23245ade080cee0b38115f0fa3468e5f
参商 <a07033686263@gmail.com>
2025-06-01 10:13:52 +0900

    Update and rename README.md to chapter_01variable/variant_learning.java

---
+++ chapter_01variable/variant_learning.java
@@ -0,0 +1,21 @@
+public class variant_learning{
+
+	public static void main(String[] args){
+
+		int c;//declare a variable
+		
+		int a = 1;//defind a variant "a" ,integer type the name is "a",the value is "1"
+		
+		int b = 3;//defind a variant "b" ,integer type the name is "b",the value is "2"
+		
+		b = 89;//Assign the value 89 to b
+		
+		System.out.println("a");//print a 
+		
+		System.out.println("b");//print b
+		
+		System.out.println(a);
+		
+		System.out.println(b);
+	}
+}
